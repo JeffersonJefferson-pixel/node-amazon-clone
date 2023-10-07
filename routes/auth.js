aa1ddf645dadffaf19c56d25dd9bb9838c324355
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2023-10-07 22:01:34 +0700

    add sign in api

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -29,6 +30,32 @@ authRouter.post("/api/signup", async (req, res) => {
     res.json(user);
   } catch (e) {
     res.status(500).json({ error: e.message })
+  }
+});
+
+authRouter.post("/api/signin", async (req, res) => {
+  try {
+    const {email, password} = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res
+        .status(404)
+        .json({ msg: "User with the email doesn't exist!"});
+        
+    }
+
+    const isMatch = await bcryptjs.compare(password, existingUser.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ msg: "Incorrect password." });
+    }
+
+    const token = jwt.sign({id: existingUser._id}, process.env.SECRET);
+
+    res.json({ token, ...existingUser._doc });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
 });
 
