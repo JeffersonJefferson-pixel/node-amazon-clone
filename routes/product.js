cc66681dfa7f682866e8b843fb1c6d0fcb01a533
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2023-10-28 11:38:12 +0700

    add search products

--- routes/product.js
+++ routes/product.js
@@ -14,4 +14,17 @@ productRouter.get("/api/products", auth, async (req, res) => {
   }
 });
 
+productRouter.get("/api/products/search/:name", auth, async (req, res) => {
+  try {
+    const { name } = req.params;
+    const products = await Product.find({
+      name: { $regex: `^${name}`, $options: "i" },
+    });
+    console.log(products);
+    res.json(products);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
 module.exports = productRouter;
